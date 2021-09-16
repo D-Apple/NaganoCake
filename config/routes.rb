fc7b40bb3218988c36b7e11ab4ed6d5ab8b44663
Uwaraba913 <metalcoola5963@gmail>
2021-09-16 07:01:52 +0000

    開発スタート

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,5 @@ Rails.application.routes.draw do
    registrations: 'end_users/registrations'
  }
  
- root to: 'homes#top'
- get 'homes/about' => 'homes#about'
- 
- get 'mypage' => 'end_users/end_users#show', as: 'mypage'
- get 'mypage/edit' => 'end_users/end_users#edit', as: 'mypage/edit'
- get 'mypage/verification' => 'end_users/end_users#verification', as: 'mypage/withdrawal'
- patch 'mypage/withdrawal' => 'end_users/end_users#withdrawal'
- 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
