6e5974897c27ad0bbd5f76d7c69a3351d06e3fe7
teppei667 <teptympei.sasstjgada.666@gmail.com>
2021-09-16 02:27:15 +0000

    migrate products

--- db/migrate/20210915075626_create_products.rb
+++ db/migrate/20210915075626_create_products.rb
@@ -1,7 +1,12 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-
+      t.integer :genre_id, null: false
+      t.boolean :is_status, null: false, default: true
+      t.string :name, null: false
+      t.text :introduction, null: false
+      t.integer :price, null: false
+      t.string :image_id, null: false
       t.timestamps
     end
   end
