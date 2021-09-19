114c2ca4d3ad39b6a9f35d66221ddb70ab13bc8e
Uwaraba913 <metalcoola5963@gmail>
2021-09-19 02:17:03 +0000

    genreとprodutのedit,show,index等作りました

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '2766bab7c4dd9e7d54e8ea4e0655f2b4b4cb6f23ba739fac89e2d60c0fe6268bd00015e754fbda2c392b95d3978d18ae3cb34b2918e9d1b5305736ef2b81d0e0'
