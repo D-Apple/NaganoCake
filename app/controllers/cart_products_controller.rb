e7d9ce893604b562a40521b1e0f90ceeec296aba
teppei667 <teptympei.sasstjgada.666@gmail.com>
2021-09-20 04:11:41 +0000

    カート機能とマイページ編集機能を実装しました

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,13 +1,19 @@
 class CartProductsController < ApplicationController
   def index
     @cart_products = CartProduct.where(end_user_id: current_end_user.id)
-
+    @total_price = 0
   end
 
   def create
     @cart_product = CartProduct.new(cart_product_params)
     @cart_product.save
     redirect_to cart_products_path
+  end
+
+  def update
+    @cart_product = CartProduct.find(params[:id])
+    @cart_product.update(cart_product_params)
+    redirect_to request.referer
   end
 
   def destroy
