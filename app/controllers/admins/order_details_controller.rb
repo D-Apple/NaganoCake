80fca7513a58c173ce59e48609b64c47743c3cd7
teppei667 <teptympei.sasstjgada.666@gmail.com>
2021-09-25 08:02:32 +0000

    status changes

--- app/controllers/admins/order_details_controller.rb
+++ app/controllers/admins/order_details_controller.rb
@@ -1,11 +1,21 @@
 class Admins::OrderDetailsController < ApplicationController
+
   before_action :authenticate_admin!
+
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order_detail.update(order_detail_params)
+    if @order_detail.production_status == "製作中"
+      @order_detail.order.update(status: "製作中")
+    end
+    @order_details = OrderDetail.where(order_id: @order_detail.order.id)
+    if @order_details.count == @order_details.where(production_status: "製作完了").count
+      @order_detail.order.update(status: "発送準備中")
+    end
+
     redirect_to request.referer
   end
-  
+
   private
   def order_detail_params
     params.require(:order_detail).permit(:production_status)
